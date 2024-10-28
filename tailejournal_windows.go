649bdbd669f2b3793a62404ccfbb785a6d031680
Victor Alfonso Guillen Zurita <victorguillen@TLALA3616592MAC-10.local>
2024-10-27 21:12:39 -0600

    Add tailejournal

---
+++ tailejournal_windows.go
@@ -0,0 +1,13 @@
+// +build windows
+
+package tailejournal
+
+import (
+	"os"
+
+	"github.com/influxdata/tail/winfile"
+)
+
+func OpenFile(name string) (file *os.File, err error) {
+	return winfile.OpenFile(name, os.O_RDONLY, 0)
+}
